b474fc8dc9470769d3c9156704faa64c2e2715e9
zaharlol <kovtun.zahar12@mail.ru>
2023-05-15 12:58:56 +0300

    Добавьте файлы проекта.

---
+++ 12.1.5/Program.cs
@@ -0,0 +1,52 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        AdsUser user = new AdsUser();
+        user.InUsers();
+    }
+}
+class User
+{
+    public string Login { get; set; }
+    public string Name { get; set; }
+    public bool IsPremium { get; set; }
+}
+class AdsUser : User
+{
+    public void InUsers()
+    {
+        Ads ads = new Ads();
+        Console.WriteLine("Логин");
+        Login = Console.ReadLine();
+        Console.WriteLine("Имя");
+        Name = Console.ReadLine();
+        Console.WriteLine(" Здравствуйте, {0}!" + "\n У вас есть подписка?", Name);
+        IsPremium = Convert.ToBoolean(Console.ReadLine());
+        if (IsPremium == false)
+        {
+            ads.ShowAds();
+        }
+        else
+        {
+            Console.WriteLine("Отлично, спасибо вам что пользуетесь нашей подпиской!");
+        }
+    }
+}
+class Ads
+{
+    public void ShowAds()
+    {
+        Console.WriteLine("Посетите наш новый сайт с бесплатными играми free.games.for.a.fool.com");
+        // Остановка на 1 с
+        Thread.Sleep(1000);
+
+        Console.WriteLine("Купите подписку на МыКомбо и слушайте музыку везде и всегда.");
+        // Остановка на 2 с
+        Thread.Sleep(2000);
+
+        Console.WriteLine("Оформите премиум-подписку на наш сервис, чтобы не видеть рекламу.");
+        // Остановка на 3 с
+        Thread.Sleep(3000);
+    }
+}
---
+++ Algorithm/Program.cs
@@ -0,0 +1,51 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+    Algoritm algoritm = new Algoritm();
+        algoritm.Lines();
+        algoritm.Sicl();
+        algoritm.Razv();
+
+        Console.ReadKey();
+    }
+}
+
+class Algoritm
+{
+    public void Lines()
+    {
+        Console.WriteLine("Как вас зовут?");
+        string name = Console.ReadLine();
+        string greetings = ("Привет, " + name);
+        Console.WriteLine(greetings);
+    }
+    public void Sicl()
+    {
+        Console.WriteLine("Сколько элементов будет в массиве?");
+        var count = Convert.ToInt32(Console.ReadLine());
+        var array = new string[count];
+        for (int i = 0; i < count; i++)
+        {
+            array[i] = Console.ReadLine();
+        }
+        Console.WriteLine("Все эллементы записаны:");
+        foreach (var item in array)
+        {
+            Console.WriteLine(item);
+        }
+    }
+    public void Razv()
+    {
+        Console.WriteLine("Введите свой возраст");
+        int age = Convert.ToInt32(Console.ReadLine());
+        if (age > 13) 
+        {
+            Console.WriteLine("Вы усспешно зарегестрированы!");
+        }
+        else
+        {
+            Console.WriteLine("Пользователи младше 14 лет не могут быть зарегестрированы");
+        }
+    }
+}
